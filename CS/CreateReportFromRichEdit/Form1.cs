a66018cba48fe801a42b4965e2b8675d7942378c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CreateReportFromRichEdit/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace CreateReportFromRichEdit {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e) {
+            OpenDocumentAndShowReport();
+        }
+
+        void OpenDocumentAndShowReport() {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                try {
+                    if (openFileDialog1.OpenFile() != null) {
+                        using (RichEditDocumentServer reds = new RichEditDocumentServer()) {
+                            reds.LoadDocument(openFileDialog1.FileName);
+
+                            using (XtraReport1 report = new XtraReport1()) {
+                                report.RichText.Rtf = reds.RtfText;
+                                report.ShowPreviewDialog();
+                            }
+                        }
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create a report from a text document via RichEditDocumentServer
+
+
+<p>This example illustrates how to create a report with a formatted content. The main idea of this approach is loading a required document to the <a href="http://documentation.devexpress.com/#CoreLibraries/clsDevExpressXtraRichEditRichEditDocumentServertopic"><u>RichEditDocumentServer</u></a> and assigning the document content to the <a href="http://documentation.devexpress.com/#XtraReports/clsDevExpressXtraReportsUIXRRichTexttopic"><u>XRRichText</u></a> component in a report.</p>
+
+<br/>
+
+
